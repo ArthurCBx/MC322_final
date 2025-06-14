3251328ddcdf616b65b1d68304c73477f69f8314
Arthur Barreto <arthur.cb102003@gmail.com>
2025-06-14 12:39:47 -0300

    Criando espaço para classes de pagamento

--- projeto_livraria/src/pagamento/Cartao.java
+++ projeto_livraria/src/pagamento/Cartao.java
@@ -1,6 +1,31 @@
-package pessoa;
+package pagamento;
 
 public class Cartao {
     private final int numero;
+    private final int codigo_de_seguranca;
+    private final String validade;
+    private final TipoCartao tipoCartao;
 
+    public Cartao(int numero, int codigo_de_seguranca, String validade, TipoCartao tipoCartao) {
+        this.numero = numero;
+        this.codigo_de_seguranca = codigo_de_seguranca;
+        this.validade = validade;
+        this.tipoCartao = tipoCartao;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getCodigo_de_seguranca() {
+        return codigo_de_seguranca;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public TipoCartao getTipoCartao() {
+        return tipoCartao;
+    }
 }
--- projeto_livraria/src/pagamento/TipoCartao.java
+++ projeto_livraria/src/pagamento/TipoCartao.java
@@ -1,4 +1,16 @@
 package pagamento;
 
 public enum TipoCartao {
+    CREDITO(200),
+    DEBITO(300);
+
+    private final int valor_limite_sem_senha;
+
+    TipoCartao(int valor_limite_sem_senha) {
+        this.valor_limite_sem_senha = valor_limite_sem_senha;
+    }
+
+    public int getValor_limite_sem_senha() {
+        return valor_limite_sem_senha;
+    }
 }
