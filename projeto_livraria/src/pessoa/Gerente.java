114269162b23c1c611ebd1bacb8a1c406dc8e857
Arthur Barreto <arthur.cb102003@gmail.com>
2025-06-19 17:45:17 -0300

    Gerente pronto

--- projeto_livraria/src/pessoa/Gerente.java
+++ projeto_livraria/src/pessoa/Gerente.java
@@ -9,17 +9,34 @@ public class Gerente extends Funcionario{
         super(nome, CPF, idade, data_nascimento, email, id, login, senha);
     }
 
+    /**
+     * Um novo produto é adicionado ao estoque
+     * @param produto Produto a ser adicionado ou alterado no estoque.
+     * @param quantidade Quantidade do produto a ser adicionada ou alterada.
+     */
     public void addNewProduto(IntProduto produto, int quantidade){
         GerenciadorEstoque.carregaProduto(produto);
         GerenciadorEstoque.appendProduto(quantidade);
     }
 
+    /**
+     * Adiciona uma quantidade de um produto já existente no estoque.
+     * @param produto Produto a ser adicionado ou alterado no estoque.
+     * @param quantidade Quantidade do produto a ser adicionada ou alterada.
+     */
     public void addProduto(IntProduto produto, int quantidade){
-        GerenciadorEstoque.addProduto(produto, quantidade);
+        GerenciadorEstoque.carregaProduto(produto);
+        GerenciadorEstoque.alteraProduto(quantidade);
     }
 
+    /**
+     * Remove uma quantidade de um produto já existente no estoque.
+     * @param produto Produto a ser removido ou alterado no estoque.
+     * @param quantidade Quantidade do produto a ser removida ou alterada.
+     */
     public void removeProduto(IntProduto produto, int quantidade){
-
+        GerenciadorEstoque.carregaProduto(produto);
+        GerenciadorEstoque.alteraProduto(-quantidade);
     }
 
 }
