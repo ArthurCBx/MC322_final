f8e78b10c185a16f6996122d1a525d77e049fc23
Vitor Albertini <v206645@dac.unicamp.br>
2025-06-19 17:48:32 -0300

    a

--- projeto_livraria/src/Gerencia/Estoque/GerenciadorEstoque.java
+++ projeto_livraria/src/Gerencia/Estoque/GerenciadorEstoque.java
@@ -34,13 +34,15 @@ public class GerenciadorEstoque {
     // Adiciona uma entrada de produto na lista de produtos (produto da propriedade)
 
     public static void appendProduto(int quantidade) {
-        getEntradas().add(new Entrada(getProduto(),quantidade,true,-1));
+        getEntradas().add(new Entrada(getProduto(), quantidade, true, -1));
     }
 
     // Remove uma entrada de produto na lista de produtos
 
     public static void deleteProduto(String id) {
         getEntradas().remove(buscaProduto(id));
+
+        GerenciadorEstoque.getEntradas().get(GerenciadorEstoque.buscaProduto(produto.getID())).getQuantiade()
     }
 
     // Busca um produto na lista de produtos
@@ -48,6 +50,7 @@ public class GerenciadorEstoque {
 
     /**
      * Busca um produto por ID na lista do estoque (entradas), retorna seu indice ou joga Produto Não
+     *
      * @param id
      * @return
      */
@@ -63,6 +66,7 @@ public class GerenciadorEstoque {
 
     /**
      * Carrega uma interface produto via ID na propriedade "produto" do Gerenciador
+     *
      * @param id um ID de produto
      */
     public static void carregaProduto(String id) {
@@ -71,6 +75,7 @@ public class GerenciadorEstoque {
 
     /**
      * Carrega uma interface produto na propriedade "produto" do Gerenciador
+     *
      * @param produto uma interface produto
      */
     public static void carregaProduto(IntProduto produto) {
@@ -88,11 +93,12 @@ public class GerenciadorEstoque {
     }
 
     /**
-     *
-     * @param quantidade
+     * Altera a quantidade de uma entrada com o minimo de zero
+     * @param quantidade soma essa quantidade na quantidade atual da entrada
      */
     public static void alteraProduto(int quantidade) {
-        getEntradas().get(getEntradas().indexOf(getProduto())).setQuantiade(quantidade);
+        int index = buscaProduto(getProduto().getID());
+        getEntradas().get(index).setQuantiade(Math.min(getEntradas().get(index).getQuantiade() + quantidade, 0));
     }
 
 }
