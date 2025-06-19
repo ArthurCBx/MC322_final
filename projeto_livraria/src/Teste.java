873cf2dc7e00d45893c12f613c6a15ab9d320401
Arthur Barreto <arthur.cb102003@gmail.com>
2025-06-19 15:16:53 -0300

    testes

--- projeto_livraria/src/Teste.java
+++ projeto_livraria/src/Teste.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import pessoa.Funcionario;
+import pessoa.Pessoa;
 
 public class Teste {
+    public static void main(String[] args) {
+        Pessoa funcionario = new Funcionario("328801007-20", 20, "01/01/2003", "abd@gmail.com", "222",21999, "abd", "123456");
+        UserManager.writeClientToFile(funcionario);
+
+    }
 }
