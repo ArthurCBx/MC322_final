35e305ac2108a68037574949c9e108590842ff7c
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-23 16:13:46 -0300

    implementando Livro

--- projeto_livraria/src/Produtos/Livro/Livro.java
+++ projeto_livraria/src/Produtos/Livro/Livro.java
@@ -2,15 +2,64 @@ package Produtos.Livro;
 
 import java.util.ArrayList;
 
-public class Livro{
+import Produtos.Produto;
+import Produtos.Propriedade;
+
+public class Livro implements Produto{
 
     private String nome;
     private float preco;
+    private String id;
     private ArrayList<Genero> genero;
+    private int quantidadeDisponivel;
+    private boolean isAlugavel;
+    private boolean isCompravel;
+    private int secao;
 
-    public Livro(String nome, float preco, ArrayList<Genero> genero) {
+    public Livro(String nome, float preco, String id, ArrayList<Genero> genero, int quantidadeDisponivel, boolean isAlugavel, int secao) {
         this.nome = nome;
         this.preco = preco;
+        this.id = id;
         this.genero = genero;
+        this.quantidadeDisponivel = quantidadeDisponivel;
+        this.isAlugavel = isAlugavel;
+        this.isCompravel = true;
+        this.secao = secao;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getID(){
+        return this.id;
+    }
+
+    public int getQuantidadeDisponivel(){
+        return this.quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel){
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public boolean isAlugavel(){
+        return this.isAlugavel;
+    }
+
+    public boolean isCompravel(){
+        return this.isCompravel;
+    }
+
+    public int getSecao(){
+        return secao;
+    }
+
+    public ArrayList<Propriedade> getPropriedades(){
+
     }
 }
