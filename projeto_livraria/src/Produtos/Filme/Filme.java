929a995d8b22c6c9d1d3aa34793ff66db701ddb1
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-23 23:32:06 -0300

    adicionando filme

--- projeto_livraria/src/Produtos/Filme/Filme.java
+++ projeto_livraria/src/Produtos/Filme/Filme.java
@@ -1,18 +1,69 @@
 package Produtos.Filme;
 
+import Produtos.Produto;
+import Produtos.Propriedade;
 import Produtos.Livro.Genero;
 
 import java.util.ArrayList;
 
-public class Filme {
+public class Filme implements Produto{
 
     private String nome;
-    private float preco;
+    private double preco;
     private ArrayList<Genero> genero;
+
+    private String id;
+    private int quantidadeDisponivel;
+    private boolean isAlugavel;
+    private boolean isCompravel;
+    private int secao;
+    private ArrayList<Propriedade> propriedades;
 
     public Filme(String nome, float preco, ArrayList<Genero> genero) {
         this.nome = nome;
         this.preco = preco;
         this.genero = genero;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public ArrayList<Genero> getGenero() {
+        return genero;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public boolean isAlugavel() {
+        return isAlugavel;
+    }
+
+    public boolean isCompravel() {
+        return isCompravel;
+    }
+
+    public int getSecao() {
+        return secao;
+    }
+
+    public ArrayList<Propriedade> getPropriedades() {
+        return propriedades;
+    }
+
+
 }
