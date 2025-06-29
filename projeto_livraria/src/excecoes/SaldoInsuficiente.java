7c1156a9f94a7fcf78ad6d3da84a49410f3bcc12
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 20:56:37 -0300

    a

--- projeto_livraria/src/excecoes/SaldoInsuficiente.java
+++ projeto_livraria/src/excecoes/SaldoInsuficiente.java
@@ -1,5 +1,8 @@
 package excecoes;
 
+/*
+ * Exceção para quando não se pode fazer uma compra dada a falta de saldo
+ */
 public class SaldoInsuficiente extends RuntimeException {
     public SaldoInsuficiente(String message) {
         super(message);
