3709e9fb16d0cb9ed3667fd986a8c8177cd20f67
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 20:20:08 -0300

    correção de comentário

--- projeto_livraria/src/Produtos/Produto.java
+++ projeto_livraria/src/Produtos/Produto.java
@@ -1,10 +1,11 @@
 package Produtos;
 
+/*
+ * Modelo a ser usado para todos os tipos de produto
+ * Nessa livraria serão comercializados três diferentes tipos de produto
+ * Sendo eles livros, filmes e genericos, como gift cards, materiais escoleres, miscelânea, etc.
+ */
 public interface Produto {
-
-
-    // so deixei criado pra poder fazer referencia a interface produto
-
     String getNome();
 
     String getId();
