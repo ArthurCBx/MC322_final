2aff7c442f09d055dd00777a38a282a7afd8705d
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 20:58:09 -0300

    commit

--- projeto_livraria/src/excecoes/SemEstoque.java
+++ projeto_livraria/src/excecoes/SemEstoque.java
@@ -1,5 +1,8 @@
 package excecoes;
 
+/*
+ * Exceção para quando não houver estoque suficiente de algum produto durante uma venda
+ */
 public class SemEstoque extends RuntimeException {
     public SemEstoque(String message) {
         super(message);
