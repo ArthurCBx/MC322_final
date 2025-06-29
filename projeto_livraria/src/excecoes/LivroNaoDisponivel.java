e31fa2f4bc421876988a5e865b2b230e259b3589
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 20:52:22 -0300

    c

--- projeto_livraria/src/excecoes/LivroNaoDisponivel.java
+++ projeto_livraria/src/excecoes/LivroNaoDisponivel.java
@@ -1,5 +1,8 @@
 package excecoes;
 
+/*
+ * Exceção quando um livro for solicitado, ele estiver cadastrado no estoque mas encontra-se indisponível
+ */
 public class LivroNaoDisponivel extends RuntimeException {
     public LivroNaoDisponivel(String message) {
         super(message);
