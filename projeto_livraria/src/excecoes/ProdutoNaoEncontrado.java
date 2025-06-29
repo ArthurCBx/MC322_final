aa54a48e38d5276ed9cf386074e7b5420d8f29ca
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 20:54:02 -0300

    modificação exceções

--- projeto_livraria/src/excecoes/ProdutoNaoEncontrado.java
+++ projeto_livraria/src/excecoes/ProdutoNaoEncontrado.java
@@ -1,5 +1,8 @@
 package excecoes;
 
+/*
+ * Exeção quando um produto não estiver cadastrado no sistema
+ */
 public class ProdutoNaoEncontrado extends RuntimeException {
     public ProdutoNaoEncontrado(String message) {
         super(message);
