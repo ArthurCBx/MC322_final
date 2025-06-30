7999c05cc794063d464254240c57d8eb5838be19
Eduardo Pereira Tejada <duduptejada@gmail.com>
2025-06-29 21:01:31 -0300

    opções de pagamento

--- projeto_livraria/src/pagamento/TipoPagamento.java
+++ projeto_livraria/src/pagamento/TipoPagamento.java
@@ -1,5 +1,8 @@
 package pagamento;
 
+/*
+ * Enumeração para especificar as opções de pagamento, podendo ser em dinheiro, no pix ou no cartão (débito ou crédito)
+ */
 public enum TipoPagamento {
     DINHEIRO,
     PIX,
